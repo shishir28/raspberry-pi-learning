1068b3d1059b1f5353610e0d593163f0fa0df680
Shishir Kumar Mishra <shishir28@gmail.com>
2022-05-16 23:29:48 +1000

    Doorbell and Alertor added

--- 06.Buzzer/06.2.Alerter/Alertor.py
+++ 06.Buzzer/06.2.Alerter/Alertor.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
 import time
-
+import math
 
 buzzerPin = 11
 buttonPin = 12
 
 
 def setup():
+    global p
     GPIO.setmode(GPIO.BOARD)
     # using physical GPIO numbering. it is used to set the serial number of GPIO which is based on physcial location of pin
     # This is GPIO17 amd GPIO18 corresponds to pin 11 and 12
@@ -15,22 +16,37 @@ def setup():
     # buttonPin to input moe with pull resistor
     GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    p = GPIO.PWM(buzzerPin, 1)
+    p.start(0)
+
 
 def loop():
     while True:
         if GPIO.input(buttonPin) == GPIO.LOW:  # if button is pressed
-            # turn it on using HIGH as output level
-            GPIO.output(buzzerPin, GPIO.HIGH)
-            print('buzzer turned on >>')
-            time.sleep(0.1)
+            alertor()
+            print('alertor turned on >>')
         else:  # if button is relessed
             # turn it off using LOW as output level
-            GPIO.output(buzzerPin, GPIO.LOW)
-            print('buzzer turned off <<<')
-            time.sleep(0.1)
+            stopAlertor()
+            print('alertor turned off <<<')
+
+
+def alertor():
+    p.start(50)
+    for x in range(0, 361):
+        sinVal = math.sin(x*(math.pi/180.0))
+        toneVal = 2000 + sinVal*500
+        p.ChangeFrequency(toneVal)
+        time.sleep(0.001)
+
+
+def stopAlertor():
+    p.stop()
+    time.sleep(0.2)
 
 
 def destroy():
+    GPIO.output(buzzerPin, GPIO.LOW)
     GPIO.cleanup()                    # Release GPIO resource
 
 
---
+++ 06.Buzzer/06.2.Alerter/Instructions.md
@@ -0,0 +1,10 @@
+# Notes
+1. Passive Buzzer x1
+2. NPN Transistor x1
+3. Register 1 kΩ x1
+4. Register 10 kΩ x2
+5. Pus Button Switch x1
+6. Raspberry Pi (with 40 GPIO) x1 
+7. GPIO Extension Board & Wire x1
+8. Breadboard x1
+9. Jumper Wire x6
